63208284a34c321790267f19421e54523ed8ff3a
John Zhou <john2001@live.ca>
2019-09-14 14:57:33 -0400

    Initial Commit

---
+++ Decision Maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Decision Maker
+//
+//  Created by John Zhou on 2019-09-14.
+//  Copyright © 2019 John Zhou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
